29b356165017860de5f07ed2f351f49d8e281d98
JoyMaya <79027614+JoyMaya@users.noreply.github.com>
2021-03-11 08:28:23 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 //% color="#AEDD81" weight=20 icon="\uf06c"
-//% groups='[ "Actuator"]'
+//% groups='[ "Actuator","Handle"]'
 namespace warrior {
     const PCA9685_ADDRESS = 0x40
 	const MODE1 = 0x00
@@ -22,6 +22,36 @@ namespace warrior {
         M2 = 10,
         M3 = 12,
         M4 = 14
+    }
+	    
+		export enum enRocker {
+        //% blockId="Nostate" block="Nostate"
+        Nostate = 0,
+        //% blockId="Up" block="Up"
+        Up,
+        //% blockId="Down" block="Down"
+        Down,
+        //% blockId="Left" block="Left"
+        Left,
+        //% blockId="Right" block="Right"
+        Right,
+        //% blockId="Press" block="Press"
+        Press
+    }
+	
+	    export enum enButtonState {
+        //% blockId="Press" block="Press"
+        Press = 0,
+        //% blockId="Realse" block="Realse"
+        Realse = 1
+    }
+    
+		export enum enButton {
+        
+        B1 = 0,
+        B2,
+        B3,
+        B4
     }
 	
     let initialized = false	
@@ -86,7 +116,7 @@ namespace warrior {
 	//% blockId=warrior_MotorRun block="Motor|%index|speed(-255~255) %speed"
     //% weight=90
     //% speed.min=-255 speed.max=255
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% group="Actuator" name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function MotorRun(index: enMotors, speed: number): void {
         if (!initialized) {
             initPCA9685()
@@ -150,6 +180,126 @@ namespace warrior {
         let v_us = (50/9) * degree + 1500 // 0.5~2.5 (2500-500)/360
         let value = v_us * 4096 / 20000
         setPwm(index, 0, value)
+    
+}
+
+    //% blockId=warrior_Rocker block="Rocker|value %value"
+    //% weight=96
+    //% blockGap=10
+    //% color="#FFCC00"
+    //% group="Handle" name.fieldEditor="gridpicker" name.fieldOptions.columns=6
+    export function Rocker(value: enRocker): boolean {
+
+        pins.setPull(DigitalPin.P8, PinPullMode.PullUp);
+        let x = pins.analogReadPin(AnalogPin.P1);
+        let y = pins.analogReadPin(AnalogPin.P2);
+        let z = pins.digitalReadPin(DigitalPin.P8);
+        let now_state = enRocker.Nostate;
+
+        if (x < 200) // 上
+        {
+
+            now_state = enRocker.Up;
+
+        }
+        else if (x > 900) //下
+        {
+
+            now_state = enRocker.Down;
+        }
+        else  // 左右
+        {
+            if (y < 200) //右
+            {
+                now_state = enRocker.Right;
+            }
+            else if (y > 900) //左
+            {
+                now_state = enRocker.Left;
+            }
+        }
+        if (z == 0)
+            now_state = enRocker.Press;
+        if (now_state == value)
+            return true;
+        else
+            return false;
+
+    }
+    
+    //% blockId=warrior_Button block="Button|num %num|value %value"
+    //% weight=95
+    //% blockGap=10
+    //% color="#FFCC00"
+    //% group="Handle" name.fieldEditor="gridpicker" name.fieldOptions.columns=5
+    export function Button(num: enButton, value: enButtonState): boolean {
+         let temp = false;
+         switch (num) {
+            case enButton.B1: {
+              pins.setPull(DigitalPin.P13, PinPullMode.PullUp);
+              if (pins.digitalReadPin(DigitalPin.P13) == value) {
+                temp = true;
+              }
+              else {
+                temp = false;
+              }
+              break;
+            }
+            case enButton.B2: {
+              pins.setPull(DigitalPin.P14, PinPullMode.PullUp);
+              if (pins.digitalReadPin(DigitalPin.P14) == value) {
+                temp = true;
+              }
+              else {
+                temp = false;
+              }
+              break;
+            }
+            case enButton.B3: {
+              pins.setPull(DigitalPin.P15, PinPullMode.PullUp);
+              if (pins.digitalReadPin(DigitalPin.P15) == value) {
+                temp = true;
+              }
+              else {
+                temp = false;
+              }
+              break;
+            }
+            case enButton.B4: {
+              pins.setPull(DigitalPin.P16, PinPullMode.PullUp);
+              if (pins.digitalReadPin(DigitalPin.P16) == value) {
+                temp = true;
+              }
+              else {
+                temp = false;
+              }
+              break;
+            }
+        }
+        return temp;         
+    }
+    
+    //% blockId==onKey block="Key %pin |Press"
+    //% weight=94
+    //% blockGap=10
+    //% color="#FFCC00"
+    //% group="Handle" name.fieldEditor="gridpicker" name.fieldOptions.columns=5
+    export function onKey(pin: enButton, body: Action): void {
+        let Pin = 0;
+        pins.setPull(DigitalPin.P13, PinPullMode.PullUp);
+        pins.setPull(DigitalPin.P14, PinPullMode.PullUp);
+        pins.setPull(DigitalPin.P15, PinPullMode.PullUp);
+        pins.setPull(DigitalPin.P16, PinPullMode.PullUp); 
+        if (pin == enButton.B1) {
+            Pin = DigitalPin.P13;
+        } else if (pin == enButton.B2) {
+            Pin = DigitalPin.P14;
+        } else if (pin == enButton.B3) {
+            Pin = DigitalPin.P15;
+        } else if (pin == enButton.B4) {
+            Pin = DigitalPin.P16;
+        }
+        pins.onPulsed(Pin, PulseValue.Low, body);
     }
 	
 
